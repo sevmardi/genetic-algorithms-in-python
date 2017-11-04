25d3fe8cfce3fcce09bcee59f11b99231988a5a9
Sevak Mardirosian <smardirosians@gmail.com>
2017-11-04 13:22:23 +0100

    new features

--- genetic.py
+++ genetic.py
@@ -4,42 +4,60 @@ extract the genetic engine code from that specific to guessing the password so i
 
 """
 
-import random 
+import random
 
 
-def _generate_parent(length, geneSet):
+class Chromosome:
+    Genes = None
+    Fitness = None
+
+    """docstring for Chromosome:"""
+
+    def __init__(self, genes, fitness):
+        self.Genes = genes
+        self.Fitness = fitness
+
+
+def _generate_parent(length, geneSet, get_fitness):
     genes = []
     while len(genes) < length:
         sampleSize = min(length - len(genes), len(geneSet))
         genes.extend(random.sample(geneSet, sampleSize))
-    return ''.join(genes)
+    genes = ''.join(genes)
+    fitness = get_fitness(genes)
 
-def _mutate(parent, geneSet):
+    return Chromosome(genes, fitness)
+
+
+def _mutate(parent, geneSet, get_fitness):
     index = random.randrange(0, len(parent))
     child_genes = list(parent)
     new_gene, alternate = random.sample(geneSet, 2)
     child_genes[index] = alternate \
         if new_gene == child_genes[index] \
         else new_gene
-    return ''.join(child_genes)
+    genes = ''.join(child_genes)
+    fitness = get_fitness(genes)
+
+    return Chromosome(genes, fitness)
 
 
-def get_best(get_fitness, targetLen,optimalFitness, geneSet, display):
-	random.seed()
-	best_parent = _generate_parent(targetLen, geneSet)
-	best_fitness = get_fitness(best_parent)
-	display(best_parent)
-	if best_fitness >= optimalFitness:
-		return best_parent
+def get_best(get_fitness, targetLen, optimalFitness, geneSet, display):
+    random.seed()
+    best_parent = _generate_parent(targetLen, geneSet)
+    best_fitness = get_fitness(best_parent)
+    display(best_parent)
+    if best_fitness >= optimalFitness:
+        return best_parent
 
-	while True:
-		child = _mutate(best_parent, geneSet)
-		child_fitness = get_fitness(child)
+    while True:
+        child = _mutate(best_parent, geneSet, get_fitness)
+        child_fitness = get_fitness(child)
 
-		if best_fitness >= child_fitness:
-			continue
-		display(child)
-		if child_fitness >= optimalFitness:
-			return child 
-		best_fitness = child_fitness
-		best_parent = child 
+        if best_fitness >= child_fitness:
+            continue
+        display(child)
+        if child_fitness >= optimalFitness:
+            return child
+        best_fitness = child_fitness
+        best_parent = child
--- guessPasswordTests.py
+++ guessPasswordTests.py
@@ -3,27 +3,30 @@ import random
 import genetic
 import unittest
 
+class Benchmark:
+    pass
+    
 
 class GuessPasswordTests(unittest.TestCase):
 
     def test_hello_world(self):
-        target = 'Hello World!'
+        target = 'I like this Algorithm 123!'
         self.guess_password(target)
 
     def guess_password(self, target):
-        gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
+        gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!1234567890."
         start_time = datetime.datetime.now()
 
         def fnGetFitness(genes):
             return get_fitness(genes, target)
 
-        def fnDisplay(genes):
-            display(genes, target, start_time)
+        def fnDisplay(canidate):
+            display(canidate, start_time)
 
         optimalFitness = len(target)
         best = genetic.get_best(fnGetFitness, len(
             target), optimalFitness, gene_set, fnDisplay)
-        self.assertEqual(best, target)
+        self.assertEqual(best.Genes, target)
 
 
 def get_fitness(genes, target):
@@ -31,10 +34,10 @@ def get_fitness(genes, target):
                if expected == actual)
 
 
-def display(genes, target, startTime):
+def display(canidate, target, startTime):
     timeDiff = datetime.datetime.now() - startTime
     fitness = get_fitness(genes, target)
-    print("{0}\t{1}\t{2}".format(genes, fitness, str(timeDiff)))
+    print("{0}\t{1}\t{2}".format(canidate.Genes, canidate.Fitness, str(timeDiff)))
 
 
 if __name__ == '__main__':
